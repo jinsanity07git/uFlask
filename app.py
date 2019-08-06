05c889fe97aa3441a5d61831f4a4cdfd193fbdd3
jinsanity07git <31701385+jinsanity07git@users.noreply.github.com>
2019-08-06 09:07:58 -0500

    external IP

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect
 
 app = Flask(__name__)
 
@@ -33,3 +33,8 @@ def terms():
 def privacy():
     title   ="Privacy"
     return render_template('privacy.html', title=title)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host= '0.0.0.0', port='20')
---
+++ externalIP.md
@@ -0,0 +1,8 @@
+* static IP
+* LAN Setup
+* Port Forwarding / Port Triggering
+
+add vedio links from youtue CS folder
+
+```
+flask run --host=192.168.1.4
