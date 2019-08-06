ceaff5e9fdaa6f018711662b98210f1a89dfaa2b
jinsanity07git <31701385+jinsanity07git@users.noreply.github.com>
2019-08-06 06:06:42 -0500

    15 creat rest of the Pages

--- app.py
+++ app.py
@@ -8,4 +8,24 @@ def hello_world():
     num_ls = [1,2,3,4]
     return render_template('index.html', title=title, num_ls = num_ls)
 
-    
+
+@app.route('/about')
+def about():
+    title   ="About Us"
+    return render_template('about.html', title=title)
+
+
+@app.route('/contact')
+def contact():
+    title   ="Contact Us"
+    return render_template('contact.html', title=title)
+
+@app.route('/terms')
+def terms():
+    title   ="Terms of Use"
+    return render_template('terms.html', title=title)
+
+@app.route('/privacy')
+def privacy():
+    title   ="Privacy"
+    return render_template('privacy.html', title=title)
