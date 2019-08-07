cfac0f77c5e631ade755eff0bbcf4b1f919a00fe
jinsanity07git <31701385+jinsanity07git@users.noreply.github.com>
2019-08-06 22:58:39 -0500

    make it better

--- app.py
+++ app.py
@@ -36,5 +36,6 @@ def privacy():
 
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run(host= '0.0.0.0', port='5000')
+    # app.debug = True
+    # app.run(host= '0.0.0.0', port='5000')
+    app.run()
