7663967c2e010903a877519803c625bb9dbef0e0
jinsanity07git <31701385+jinsanity07git@users.noreply.github.com>
2019-08-07 20:40:53 -0500

    file upload running

--- app.py
+++ app.py
@@ -1,41 +1,8 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask
+
+UPLOAD_FOLDER = 'D:/uploads'
 
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    title   ="homepage"
-    num_ls = [1,2,3,4]
-    return render_template('index.html', title=title, num_ls = num_ls)
-
-@app.route('/lang/<name>')
-def lang(name):
-    title   = name
-    return render_template('lang.html', title=title)
-
-@app.route('/about')
-def about():
-    title   ="About Us"
-    return render_template('about.html', title=title)
-
-
-@app.route('/contact')
-def contact():
-    title   ="Contact Us"
-    return render_template('contact.html', title=title)
-
-@app.route('/terms')
-def terms():
-    title   ="Terms of Use"
-    return render_template('terms.html', title=title)
-
-@app.route('/privacy')
-def privacy():
-    title   ="Privacy"
-    return render_template('privacy.html', title=title)
-
-
-if __name__ == "__main__":
-    # app.debug = True
-    # app.run(host= '0.0.0.0', port='5000')
-    app.run()  
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
---
+++ flaskupload.md
@@ -0,0 +1,11 @@
+router side
+* static IP
+* LAN Setup
+* Port Forwarding / Port Triggering
+
+file upload
+
+* [Flask File Uploading - Tutorialspoint](https://www.google.com/search?q=tutorialspoint+flask+file+uploading&oq=tutorialpoint+flask+&aqs=chrome.3.69i57j0l5.13899j0j4&sourceid=chrome&ie=UTF-8) 
+add vedio links from youtue CS folder
+* [Python Flask File Upload Example](https://www.roytuts.com/python-flask-file-upload-example/)
+ * [download source code.](https://github.com/roytuts/python/tree/master/python_flask_file_upload)
